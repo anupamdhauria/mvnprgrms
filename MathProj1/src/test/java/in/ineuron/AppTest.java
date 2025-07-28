b0f955348272d78e4071c9d5fac4790609ba5f4a
anupamdhauria <anupamdhauria97@gmail.com>
2025-07-28 08:46:47 +0530

    mvn manual testcases written and external plugin use

--- MathProj1/src/test/java/in/ineuron/AppTest.java
+++ MathProj1/src/test/java/in/ineuron/AppTest.java
@@ -1,20 +1,39 @@
 package in.ineuron;
 
+import static org.junit.Assert.assertEquals;
 import static org.junit.Assert.assertTrue;
+import org.junit.*;
+import in.ineuron.*;
+import junit.framework.TestCase;
 
-import org.junit.Test;
+public class AppTest extends TestCase {
 
-/**
- * Unit test for simple App.
- */
-public class AppTest 
-{
-    /**
-     * Rigorous Test :-)
-     */
-    @Test
-    public void shouldAnswerWithTrue()
-    {
-        assertTrue( true );
+    public void testSumWithPositiveNumber() {
+        MathApp ar = new MathApp();
+        int actual = ar.add(10, 20);
+        int expected = 30;
+        assertEquals(actual, expected);
     }
+
+    public void testSumWithNegativeNumber() {
+        MathApp ar = new MathApp();
+        int actual = ar.add(-10, -20);
+        int expected = -30;
+        assertEquals(actual, expected);
+    }
+
+    public void testSumWithMixedNumber() {
+        MathApp ar = new MathApp();
+        int actual = ar.add(-10, 20);
+        int expected = 10;
+        assertEquals(actual, expected);
+    }
+
+    public void testSumWithZero() {
+        MathApp ar = new MathApp();
+        int actual = ar.add(0, 0);
+        int expected = 0;
+        assertEquals(actual, expected);
+    }
+
 }
